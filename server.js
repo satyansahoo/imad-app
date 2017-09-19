a19deef3f5229eef8848a9f08b9b1de9115db8e7
satyansahoo <30659710+satyansahoo@users.noreply.github.com>
2017-09-19 16:52:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,14 +5,77 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne = {
+    title: 'Article One | Satya Sahoo',
+    heading: 'Article One',
+    date: 'sep 5,2017',
+    content: `
+          <p>
+            satya sahoo satya sahoo satya sahoo satya sahoosatya sahoo satya sahoosatya sahoo satya sahoo
+            satya sahoo satya sahoosatya sahoo satya sahoosatya sahoo satya sahoo
+            satya sahoo satya sahoosatya sahoo satya sahoosatya sahoo satya sahoo
+          </p>
+          <p>
+            satya sahoo satya sahoo satya sahoo satya sahoosatya sahoo satya sahoosatya sahoo satya sahoo
+            satya sahoo satya sahoosatya sahoo satya sahoosatya sahoo satya sahoo
+            satya sahoo satya sahoosatya sahoo satya sahoosatya sahoo satya sahoo
+          </p>
+          <p>
+            satya sahoo satya sahoo satya sahoo satya sahoosatya sahoo satya sahoosatya sahoo satya sahoo
+            satya sahoo satya sahoosatya sahoo satya sahoosatya sahoo satya sahoo
+            satya sahoo satya sahoosatya sahoo satya sahoosatya sahoo satya sahoo
+          </p> `
+     
+    
+};
+
+function createTemplete (data) {
+    var titile = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+
+    
+    var htmlTemplete =`
+     <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+           <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <hr/>
+            <h3>
+                ${heading}
+            </h3>
+            <div>
+               ${date}
+            </div>
+            <div>
+               ${content}
+            </div>
+          </div>
+        </body>
+    </html>
+  `;
+  return htmlTemplete;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-
+  res.send(createTemplete(articleOne));
+ });
 app.get('/article-two', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
